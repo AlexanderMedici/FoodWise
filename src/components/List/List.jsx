bde94127233dcf6b479ce3220a89b4dfa87e306f
Alexander M <contactimedici@gmail.com>
2021-08-17 21:56:23 -0400

    working-Map

---
+++ src/components/List/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import useStyles from "./styles.js";
+const List = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <h1>List</h1>
+    </div>
+  );
+};
+export default List;
---
+++ src/components/PlaceDetails/PlaceDetails.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import useStyles from "./styles.js";
+
+const PlaceDetails = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <h1>Detail</h1>
+    </div>
+  );
+};
+export default PlaceDetails;
